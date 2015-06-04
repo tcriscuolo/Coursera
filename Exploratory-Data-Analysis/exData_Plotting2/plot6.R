e963867a28e89b7d25c63b97339ab1991906740d
Tulio Criscuolo <tcriscuolo@gmail.com>
2015-06-04 15:19:26 -0400

    Fixed Labels
    Fixed labels for plot 6.

--- Exploratory-Data-Analysis/exData_Plotting2/plot6.R
+++ Exploratory-Data-Analysis/exData_Plotting2/plot6.R
@@ -11,9 +11,11 @@ motorVehicleIds <- SCC$SCC[motorVehiclePos];
 data <- NEI[NEI$fips %in% c("24510", "06037"), ];
 data <- data[data$SCC %in% motorVehicleIds, ];
 
+data$fips <- ifelse(data$fips == "24510","Baltimore", "Los Angeles")
+
 info <- aggregate(Emissions ~ year + fips, data = data, sum, na.rm = TRUE);
 
-q <- qplot(year, Emissions, data = info, facets = . ~ fips)
-q <- q + geom_line()+ ggtitle("Total emission of PM2.5 from motor vehicles in Baltimore");
+q <- qplot(year, Emissions, data = info, facets = . ~ fips);
+q <- q + geom_line()+ ggtitle("Total emission of PM2.5 from motor vehicles in Baltimore and Los Angeles");
 plot(q)
 dev.off();

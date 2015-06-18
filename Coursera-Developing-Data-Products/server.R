4a34604fe0b047480ee4ae487fd2421b84086094
Tulio Criscuolo <tcriscuolo@gmail.com>
2015-06-18 14:26:42 -0400

    App
    — Completed shiny app
    — Needs documentation

--- Coursera-Developing-Data-Products/server.R
+++ Coursera-Developing-Data-Products/server.R
@@ -7,47 +7,117 @@
 library(shiny)
 library(data.table)
 library(leaflet)
+library(scales)
 
 
-load(file = "data/data.Rda") # load the data set as Rda, much faster than loading from a csv file
-load("data/countyList.Rda")
-
-
+load("data/data.Rda") # load the data set as Rda, much faster than loading from a csv file
+load("data/stateList.Rda")
 
 shinyServer(function(input, output, clientData, session) {
   observe({
+    curState <- input$stateCode
+    if(curState != "All") {
+      countyList <- sort(data[, State.Code, County.Code][State.Code == curState, County.Code])
+      updateSelectInput(session, "countyNumber", choices = c("All", countyList))
+    } else {
+      updateSelectInput(session, "countyNumber", choices = "--")
+    }   
+  })
+  
+  
+  observe({
+    # On selecting a state 
+    # it will be able to select a county
+    # After selecting na county
+    # it will be able to select a site 
+    
     curCounty <- input$countyNumber
+    
+    if(curCounty == "--") 
+      return ()
+    # The state has already been selected 
+    # so it is different of All
+    curState <- input$stateCode
+    
     if(curCounty != "All") {
-      siteList <- sort(data[, County.Code, Site.Num][County.Code == 1, Site.Num])
+      siteList <- sort(data[State.Code == curState & County.Code == curCounty, ][, Site.Num])
       updateSelectInput(session, "siteNumber", choices = c("All", siteList))
     } else {
       updateSelectInput(session, "siteNumber", choices = "--")
-    }
+    }   
   })
   
-  output$dataTable <- renderDataTable({
+ 
+  
+  dataInput <- reactive({
+    # Returns the data table 
+    # according to the parameters 
+    curState <- input$stateCode
     curCounty <- input$countyNumber
     curSite <- input$siteNumber
     curSort <- input$sortBy
     curOrd <- switch(input$order, "Increasing" = FALSE, "Decreasing" = TRUE)
+    
+    if(curState == "All") {
+      ifelse(curSort == "Ozone", 
+             return(data[, ][order(OZONE, decreasing = curOrd)]),
+             return(data[, ][order(AQI, decreasing = curOrd)])) 
+    }
+    
     if(curCounty == "All") {
       ifelse(curSort == "Ozone", 
-                    return(data[, ][order(OZONE, decreasing = curOrd)]),
-                    return(data[, ][order(AQI, decreasing = curOrd)]))
+             return(data[State.Code == curState, ][order(OZONE, decreasing = curOrd)]),
+             return(data[State.Code == curState, ][order(AQI, decreasing = curOrd)]))
     } else {
       if(curSite == "All") {
         ifelse(curSort == "Ozone", 
-               return(data[County.Code == curCounty, ][order(OZONE, decreasing = curOrd)]),
-               return(data[County.Code == curCounty, ][order(AQI, decreasing = curOrd)]))
+               return(data[State.Code == curState &
+                             County.Code == curCounty, ][order(OZONE, decreasing = curOrd)]),
+               return(data[State.Code == curState & 
+                             County.Code == curCounty, ][order(AQI, decreasing = curOrd)]))
       } else {
         ifelse(curSort == "Ozone", 
-               return(data[County.Code == curCounty & Site.Num == curSite, ][order(OZONE, decreasing = curOrd)]),
-               return(data[County.Code == curCounty & Site.Num == curSite, ][order(AQI, decreasing = curOrd)]))
+               return(data[State.Code == curState & 
+                             County.Code == curCounty &
+                             Site.Num == curSite, ][order(OZONE, decreasing = curOrd)]),
+               return(data[State.Code == curState & 
+                             County.Code == curCounty & 
+                             Site.Num == curSite, ][order(AQI, decreasing = curOrd)]))
       }
     }
     
-  }, options = list(searching = FALSE))
+  })
   
- 
+  # Render the data table 
+  output$dataTable <- renderDataTable({
+    return (dataInput())
+    
+  }, options = list(searching=FALSE, ordering=FALSE, paging=TRUE, search = FALSE))
 
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
+      setView(lng = -90.85, lat = 48.45, zoom = 3)
+  })
+  
+  observe({
+    curState <- input$stateCode
+    # it will not plot all sites because it is too slow
+    if(curState == "All") {
+      leafletProxy("map", deferUntilFlush = FALSE) %>% 
+        clearMarkers()
+      return()
+    } 
+    
+    data <- dataInput()
+    coordVect <- as.data.frame(data[, Longitude, Latitude])
+    addr <- as.data.frame(data[ , Address])
+    leafletProxy("map", deferUntilFlush = FALSE) %>% 
+                        clearMarkers() %>%
+                        addMarkers(lng=coordVect$Longitude, lat=coordVect$Latitude)
+    
+  })
 })
--- Coursera-Developing-Data-Products/ui.R
+++ Coursera-Developing-Data-Products/ui.R
@@ -1,16 +1,10 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(leaflet)
 library(data.table)
 
 ## Load the data and useful lists
 load("data/data.Rda")
-load("data/countyList.Rda")
+load("data/stateList.Rda")
 
 
 
@@ -19,37 +13,51 @@ shinyUI(fluidPage(
   
 
   # Sidebar with a slider input for number of bins
-  fluidRow(
-    column(2,
-           selectInput("countyNumber", 
-                       "County Code", 
-                       choices = c("All", countyList), 
-                       selected = "All")
-    ),
-    column(2,
-           selectInput("siteNumber", 
-                       "Site Number", 
-                       choices = c("--"), 
-                       selected = "--")
-    ),
-    column(2,
-           selectInput("sortBy", 
-                       "Sort by", 
-                       choices = c("Ozone", "AQI"), 
-                       selected = "Ozone")
-    ), 
-    column(2,
-           selectInput("order", 
-                       "Order", 
-                       choices = c("Increasing", "Decreasing"), 
-                       selected = "Increasing")
+  mainPanel(
+      fluidRow(  
+        column(12,
+               leafletOutput(outputId = "map" , height = 350, width = "140%"))
+        ),
+      br(),
+      fluidRow(
+        fluidRow(
+          
+          column(2,
+                 selectInput("stateCode", 
+                             "State Code", 
+                             choices = c("All", stateList), 
+                             selected = "All")
+          ),
+          
+          column(3,
+                 selectInput("countyNumber", 
+                             "County Code", 
+                             choices = c("--"), 
+                             selected = "--")
+          ),
+          column(3,
+                 selectInput("siteNumber", 
+                             "Site Number", 
+                             choices = c("--"), 
+                             selected = "--")
+          ),
+          column(3,
+                 selectInput("sortBy", 
+                             "Sort by", 
+                             choices = c("Ozone", "AQI"), 
+                             selected = "Ozone")
+          ), 
+          column(3,
+                 selectInput("order", 
+                             "Order", 
+                             choices = c("Increasing", "Decreasing"), 
+                             selected = "Increasing")
+          )
+        ),
+       column(12, 
+          dataTableOutput(outputId="dataTable")
+       ) 
     )
-    
-    
-  ),
-  fluidRow(
-    dataTableOutput(outputId="dataTable")
-  ) ,
-
-  
-))
+  )
+)
+)
